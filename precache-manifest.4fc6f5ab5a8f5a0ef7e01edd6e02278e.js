26c375a2ac728c0c5dda0f2e573f2bbde475f33d
bnddiep <bach.nhu.diep@gmail.com>
2020-10-13 20:00:54 -0700

    Updates

---
+++ precache-manifest.4fc6f5ab5a8f5a0ef7e01edd6e02278e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac0f69981fa5cd9caf66f27a6b140f8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88b770877a336cb69a74",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "253c002f69d628ff27c2",
+    "url": "/portfolio/static/css/main.8cbdb7b0.chunk.css"
+  },
+  {
+    "revision": "88b770877a336cb69a74",
+    "url": "/portfolio/static/js/2.52ce56a3.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/portfolio/static/js/2.52ce56a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253c002f69d628ff27c2",
+    "url": "/portfolio/static/js/main.df7956aa.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c03b512250116c030b4e79167acae244",
+    "url": "/portfolio/static/media/illustrator.c03b5122.svg"
+  },
+  {
+    "revision": "b8b18a8e1bde37f95c2dde3ca144b03d",
+    "url": "/portfolio/static/media/photoshop.b8b18a8e.svg"
+  },
+  {
+    "revision": "c9df0bf2eb5fe8dd0335fd480eea5839",
+    "url": "/portfolio/static/media/postgresql.c9df0bf2.svg"
+  },
+  {
+    "revision": "0e58a87ca19092c95fd5119751533c7c",
+    "url": "/portfolio/static/media/profilephoto_temp.0e58a87c.jpg"
+  },
+  {
+    "revision": "ea795c13c6108a1e3808cd66ca9f8db2",
+    "url": "/portfolio/static/media/robot.ea795c13.png"
+  },
+  {
+    "revision": "fa4f3db7ccea840e4c2c4bca3b821123",
+    "url": "/portfolio/static/media/ruby-on-rails.fa4f3db7.svg"
+  },
+  {
+    "revision": "5a5454598479bf0f11b16bcf6edd187e",
+    "url": "/portfolio/static/media/thumbnail_1.5a545459.jpeg"
+  },
+  {
+    "revision": "d9ae92a5763ff102d74a946fc1aeadcf",
+    "url": "/portfolio/static/media/thumbnail_2.d9ae92a5.jpeg"
+  },
+  {
+    "revision": "c160e4add3003d63f1d5a606dd275d4d",
+    "url": "/portfolio/static/media/thumbnail_3.c160e4ad.jpeg"
+  },
+  {
+    "revision": "c31df9e00269be851ab4c017e33a4272",
+    "url": "/portfolio/static/media/thumbnail_4.c31df9e0.jpeg"
+  },
+  {
+    "revision": "4de982085d2a4355ea78b0994e3bb5fe",
+    "url": "/portfolio/static/media/zoom.4de98208.svg"
+  }
+]);
