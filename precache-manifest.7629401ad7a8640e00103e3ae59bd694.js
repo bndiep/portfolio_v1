96f0f99737fbe704d7cb58662f4d7246a5638b94
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-04-07 15:23:03 -0700

    Updates

---
+++ precache-manifest.7629401ad7a8640e00103e3ae59bd694.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca705fdccd4f4f7c7a2330d659473a7b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "444555f6007856b25e85",
+    "url": "/portfolio/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "53210eda180fe73eba94",
+    "url": "/portfolio/static/css/main.33060891.chunk.css"
+  },
+  {
+    "revision": "444555f6007856b25e85",
+    "url": "/portfolio/static/js/2.3bf459bd.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/portfolio/static/js/2.3bf459bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53210eda180fe73eba94",
+    "url": "/portfolio/static/js/main.307f8abd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d1005ac09978561e3167765bfb3da3a8",
+    "url": "/portfolio/static/media/Bach_Diep_Resume.d1005ac0.pdf"
+  },
+  {
+    "revision": "c03b512250116c030b4e79167acae244",
+    "url": "/portfolio/static/media/illustrator.c03b5122.svg"
+  },
+  {
+    "revision": "b8b18a8e1bde37f95c2dde3ca144b03d",
+    "url": "/portfolio/static/media/photoshop.b8b18a8e.svg"
+  },
+  {
+    "revision": "547efa0fd9457a79f7b8ee118e9c4038",
+    "url": "/portfolio/static/media/pig-latin.547efa0f.png"
+  },
+  {
+    "revision": "c9df0bf2eb5fe8dd0335fd480eea5839",
+    "url": "/portfolio/static/media/postgresql.c9df0bf2.svg"
+  },
+  {
+    "revision": "5fc198f8409b3bce015a1c838fff01f0",
+    "url": "/portfolio/static/media/profilephoto_temp.5fc198f8.jpg"
+  },
+  {
+    "revision": "714c8f600344caef5cae1b3c63fb0160",
+    "url": "/portfolio/static/media/rails-blog.714c8f60.png"
+  },
+  {
+    "revision": "c15a2ad5b117234eb0bb7ec0a2b723d6",
+    "url": "/portfolio/static/media/react-here.c15a2ad5.png"
+  },
+  {
+    "revision": "ea795c13c6108a1e3808cd66ca9f8db2",
+    "url": "/portfolio/static/media/robot.ea795c13.png"
+  },
+  {
+    "revision": "fa4f3db7ccea840e4c2c4bca3b821123",
+    "url": "/portfolio/static/media/ruby-on-rails.fa4f3db7.svg"
+  },
+  {
+    "revision": "d9b44c81d4c6486327ba9062fcaf88b6",
+    "url": "/portfolio/static/media/tic-tac-toe.d9b44c81.png"
+  },
+  {
+    "revision": "4de982085d2a4355ea78b0994e3bb5fe",
+    "url": "/portfolio/static/media/zoom.4de98208.svg"
+  }
+]);
